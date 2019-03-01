c927cad1a9a515a446a8196552c4613e53af8484
aperturesecurity <tatrasiel7@gmail.com>
2019-03-01 19:58:50 +0200

    make sure nothig is left

---
+++ build32/CMakeFiles/devilution_pch.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/devilution_pch"
+  "cotire/devilution_C_prefix.h.gch"
+  "cotire/devilution_CXX_prefix.hxx.gch"
+  "cotire/devilution_C_prefix.h"
+  "cotire/devilution_CXX_prefix.hxx"
+  "cotire/devilution_C_prefix.c"
+  "cotire/devilution_C_prefix.c.log"
+  "cotire/devilution_CXX_prefix.cxx"
+  "cotire/devilution_CXX_prefix.cxx.log"
+  "cotire/devilution_C_unity.c"
+  "cotire/devilution_CXX_unity.cxx"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/devilution_pch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
